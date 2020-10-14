c699f41017bea716f1a5dc79bdd56a85e8820b82
Arley Duarte <arleymauricio@gmail.com>
2020-10-14 15:23:44 -0300

    jest+enzyme

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,11 +4,19 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import App from '../src/App';
+import { shallow } from 'enzyme';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+
+// Using Jest + Enzyme
+describe('<App />', () => {
+  it('renders correctly, test using Jest + Enzyme', () => {
+    expect(shallow(<App/>)).toMatchSnapshot();
+  });
 });
