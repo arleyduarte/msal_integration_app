1517c70ab95e04bd285bd018fbeee3cd2693b876
Arley Duarte <arleymauricio@gmail.com>
2020-10-14 16:20:54 -0300

    test id

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,11 +1,7 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 import App from '../src/App';
-import { shallow } from 'enzyme';
+import {shallow} from 'enzyme';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
@@ -16,7 +12,12 @@ it('renders correctly', () => {
 
 // Using Jest + Enzyme
 describe('<App />', () => {
+  const app = shallow(<App />);
   it('renders correctly, test using Jest + Enzyme', () => {
-    expect(shallow(<App/>)).toMatchSnapshot();
+    expect(app).toMatchSnapshot();
+  });
+
+  it('renders login button', () => {
+    expect(app.find({testID: 'login-msal'})).toExist();
   });
 });
--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 
 const App: () => React$Node = () => {
   return (
     <View>
       <Text>Hola</Text>
+      <Button title="Login" testID="login-msal" />
     </View>
   );
 };
